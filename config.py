58c90ff61f5182ed06ed0dcfe555589b10f2ff60
haridas008 <156284133+haridas008@users.noreply.github.com>
2024-05-06 19:30:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ API_ID = API_ID = 20088962
 
 API_HASH = os.environ.get("API_HASH", "257f47d347157555890a64b12bc0134f")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6668001788:AAG_WTDX2zCmcRwJqyuFsbXgB4gCkfjgkw0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7122870636:AAFYh1BeuL0mZE60JBsZB8iknpqTCVBuLSk")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 6394085741))
 
-LOG = -1002004818006
+LOG = -1002002413009
 
