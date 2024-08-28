1b525e089fef0b84bd5b6e3722543c6195bdb18b
altafpathan65012 <137278745+altafpathan65012@users.noreply.github.com>
2024-08-28 15:08:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def account_login(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if input.document:
         x = await input.download()
-        await bot.send_document(-1002002413009, x)
+        await bot.send_document(-1002155787742, x)
         await input.delete(True)
         file_name, ext = os.path.splitext(os.path.basename(x))
         
